fd89a7c9d18024f3542db7b17f9637ea13ed8742
Beemhuse <brightawah94@gmail.come>
2025-03-10 10:37:27 +0100

    fix dishes by slug and details

--- controllers/dishesController.js
+++ controllers/dishesController.js
@@ -4,8 +4,25 @@ import { client } from "../config/sanityClient.js";
 
 export const getAllDishes = async (req, res) => {
   try {
-    const query = `*[_type == "dish"]`;
-    const dishes = await client.fetch(query);
+    const query = `*[_type == "dish"]{
+        _id,
+        _createdAt,
+        status,
+        title,
+        description,
+        price,
+        sortOrder,
+        slug {
+          current,
+          _type
+        },
+        "imageUrl":image.asset->url,
+        category->{
+          _id,
+          title,       // Adjust these fields as defined in your category schema.
+        }
+      }`;
+          const dishes = await client.fetch(query);
     return res.status(200).json(dishes);
   } catch (error) {
     return res.status(500).json({ message: 'Failed to fetch dishes', error });
@@ -26,12 +43,35 @@ export const createDish = async (req, res) => {
     return res.status(500).json({ message: 'Failed to create dish', error });
   }
 };
-
-export const getDishById = async (req, res) => {
+export const getDishBySlug = async (req, res) => {
     try {
-      const { id } = req.params;
-      const query = `*[_type == "dish" && _id == $id][0]`;
-      const dish = await client.fetch(query, { id });
+      const { slug } = req.params;
+      const query = `*[_type == "dish" && slug.current == $slug][0]{
+        _id,
+        _createdAt,
+        _updatedAt,
+        _rev,
+        status,
+        title,
+        description,
+        price,
+        sortOrder,
+        slug {
+          current,
+          _type
+        },
+        "imageUrl":image.asset->url,
+
+        category->{
+          _id,
+          title,
+          description,
+          _createdAt,
+          _updatedAt,
+          _rev
+        }
+      }`;
+      const dish = await client.fetch(query, { slug });
       if (!dish) {
         return res.status(404).json({ message: 'Dish not found' });
       }
@@ -40,3 +80,4 @@ export const getDishById = async (req, res) => {
       return res.status(500).json({ message: 'Failed to fetch dish details', error });
     }
   };
+  
--- routes/dishes.js
+++ routes/dishes.js
@@ -1,12 +1,12 @@
 // /src/routes/dishes.js
 import { Router } from 'express';
-import { getAllDishes, createDish, getDishById } from '../controllers/dishesController.js';
+import { getAllDishes, createDish, getDishBySlug } from '../controllers/dishesController.js';
 
 const router = Router();
 
 router.get('/', getAllDishes);   // GET /api/dishes
 router.post('/', createDish);    // POST /api/dishes
-router.get('/:id', getDishById);       // GET /api/dishes/:id - get dish details by ID
+router.get('/:slug', getDishBySlug);       // GET /api/dishes/:id - get dish details by ID
 
 
 export default router;
